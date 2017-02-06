72e72d71b063b9313847d70b915547414c4d0333
Mika Lindell <mika.lindell82@gmail.com>
2017-02-06 17:44:53 +0200

    All font are now included in the package

--- src/vendor.jsx
+++ src/vendor.jsx
@@ -1,4 +1,5 @@
 // Import vendors for example jQuery, Lodash or Bootstrap
 // You can import js, css, sass, ...
 
-import '../public/fonts/riogrande.scss';
+import '../public/fonts/breeserif/breeserif.scss';
+import '../public/fonts/riogrande/riogrande.scss';
